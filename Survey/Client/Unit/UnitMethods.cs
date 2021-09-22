1d42dd2ed8a1d701ee3158bc6183f6ae2d2b8f55
Zsombor Veres-Lakos <vl.zsombor@gmail.com>
2021-09-22 21:54:22 +0200

    New repository intit commit

---
+++ Survey/Client/Unit/UnitMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Survey.Client.Unit
+{
+    public class UnitMethods
+    {
+
+        public static string ChooseA(string textInput)
+        {
+            return textInput.ToUpper();
+        }
+
+    }
+}
---
+++ Survey/Server/Controllers/CardApiController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Survey.Shared.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Survey.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardApiController : ControllerBase
+    {
+
+        List<CardModel> CardList = Program.CardList;
+
+        [HttpGet]
+        [Route("Cards")]
+        public List<CardModel> GetCardsAsync()
+        {
+            return CardList;
+        }
+        [HttpGet]
+        [Route("Card")]
+        public CardModel GetCardAsync(int numberOfPost, int starindex)
+        {
+            return CardList[1];
+        }
+        [HttpPut]
+        [Route("UpdateCardRating")]
+        public async Task<int> UpdateCardRating([FromBody] CardModel card)
+        {
+            Program.CardList.Where(x => x.Id == card.Id).FirstOrDefault().Rating = card.Rating;
+
+            Program.CardList.ForEach(x => Console.WriteLine(x.Rating));
+
+            return 0;
+        }
+
+        [HttpPost]
+        public async Task<int> AddCard([FromBody] CardModel cardModel)
+        {
+
+            Program.CardList.Add(cardModel);
+
+            return cardModel.Id;
+        }
+
+    }
+}
