7fe34dc6f241d1bca5d4be946ebced2d8430e931
vlzsombor <44325704+vlzsombor@users.noreply.github.com>
2021-12-25 18:50:26 +0100

    Export pdf

--- Survey/Client/Pages/App/Board/Summary.razor.cs
+++ Survey/Client/Pages/App/Board/Summary.razor.cs
@@ -4,33 +4,78 @@ using Survey.Shared.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Grid;
+using Syncfusion.Drawing;
+using Syncfusion.Pdf.Graphics;
+using Survey.Client.Unit;
 
+using System.IO;
 namespace Survey.Client.Pages.App.Board
 {
     public partial class Summary : ComponentBase
     {
 
         public List<(CardModel, Dictionary<int, int>)> ToShowList = new List<(CardModel, Dictionary<int, int>)>();
+        
+        
+        [Inject]
+        public Microsoft.JSInterop.IJSRuntime JS { get; set;  }
 
         public void test2()
         {
-            foreach (var cardItem in CardList ?? Enumerable.Empty<CardRatingDto>())
-            {
-                var b2 = cardItem.CardModel.Rating
-                    .GroupBy(x => x.RatingNumber)
-                    .ToDictionary(grp => grp.Key,
-                     grp => grp.Count());
-
-
-                for(int i = 0; i<7; i++)
-                {
-                        b2.TryAdd(i, 0);
-                }
-
-
-                Console.WriteLine(b2);
-
-            }
         }
+
+        public void ExportToPdf()
+        {
+            int paragraphAfterSpacing = 8;
+            int cellMargin = 8;
+            PdfDocument pdfDocument = new PdfDocument();
+            //Add Page to the PDF document.
+            PdfPage page = pdfDocument.Pages.Add();
+
+            //Create a new font.
+            PdfStandardFont font = new PdfStandardFont(PdfFontFamily.TimesRoman, 16);
+
+            //Create a text element to draw a text in PDF page.
+            PdfTextElement title = new PdfTextElement("Weather Forecast", font, PdfBrushes.Black);
+            PdfLayoutResult result = title.Draw(page, new PointF(0, 0));
+
+
+            PdfStandardFont contentFont = new PdfStandardFont(PdfFontFamily.TimesRoman, 12);
+            PdfTextElement content = new PdfTextElement("This component demonstrates fetching data from a client side and Exporting the data to PDF document using Syncfusion .NET PDF library.", contentFont, PdfBrushes.Black);
+            PdfLayoutFormat format = new PdfLayoutFormat();
+            format.Layout = PdfLayoutType.Paginate;
+
+            //Draw a text to the PDF document.
+            result = content.Draw(page, new RectangleF(0, result.Bounds.Bottom + paragraphAfterSpacing, page.GetClientSize().Width, page.GetClientSize().Height), format);
+
+            //Create a PdfGrid.
+            PdfGrid pdfGrid = new PdfGrid();
+            pdfGrid.Style.CellPadding.Left = cellMargin;
+            pdfGrid.Style.CellPadding.Right = cellMargin;
+
+            //Applying built-in style to the PDF grid
+            pdfGrid.ApplyBuiltinStyle(PdfGridBuiltinStyle.GridTable4Accent1);
+
+            //Assign data source.
+            pdfGrid.DataSource = CardList;
+
+            pdfGrid.Style.Font = contentFont;
+
+            //Draw PDF grid into the PDF page.
+            pdfGrid.Draw(page, new Syncfusion.Drawing.PointF(0, result.Bounds.Bottom + paragraphAfterSpacing));
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            // Save the PDF document.
+            pdfDocument.Save(memoryStream);
+
+            // Download the PDF document
+            JS.SaveAs("Sample.pdf", memoryStream.ToArray());
+        }
+
+
     }
+
 }
---
+++ Survey/Client/Unit/FileUtil.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace Survey.Client.Unit
+{
+    public static class FileUtil
+    {
+
+        public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, byte[] data)
+                    => js.InvokeAsync<object>(
+                "saveAsFile",
+                filename,
+                    Convert.ToBase64String(data)
+                    );
+
+    }
+}
