f54001d9726d651adb4458ebe8baedf70ad1e4ca
vlzsombor <44325704+vlzsombor@users.noreply.github.com>
2022-05-07 22:30:11 +0200

    Finalizing

--- Survey/Server/Startup.cs
+++ Survey/Server/Startup.cs
@@ -91,14 +91,40 @@ namespace Survey.Server
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+
             app.UseRouting();
 
-            app.UseEndpoints(endpoints=>
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chathub");
+                endpoints.MapFallbackToFile("index.html");
             });
+
+            await Seeding(app);
+
         }
 
         private static async Task Seeding(IApplicationBuilder app)
